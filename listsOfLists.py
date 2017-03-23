a3ef5e484910bec750adde46bde4b69ba943e18f
carloscesar182 <carloscesar182@gmail.com>
2017-03-22 21:43:48 -0300

    lists of lists using for and if statement

---
+++ listsOfLists.py
@@ -0,0 +1,15 @@
+# It is a list within a list within a list
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman",
+           ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+for each_item in movies:  # Processing the movies list
+    if isinstance(each_item, list):  # To check if the current item is a list
+        for nested_item in each_item:  # If it is a list, use another 'for' loop to process the nested list
+            if isinstance(nested_item, list):  # It will check inside of your first list if have another ones
+                for deeper_item in nested_item:
+                    print(deeper_item)
+            else:
+                print(nested_item)  # If the current item of the enclosing list isn´t a list, display it on screen
+    else:
+        print(each_item)
