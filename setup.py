d105fdd06dfa3f3ba9b75db3e5690b1faa0c4a63
carloscesar182 <carloscesar182@gmail.com>
2017-03-24 22:59:54 -0300

    how to distribute your code

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Import the setup function from Python´s distribution utilities
+from distutils.core import setup
+
+"""On the left side are the setup function´s argument names.
+On the right side, your module´s metadata with de setup function´s arguments."""
+setup(
+    name='nester',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='carloscesar182',
+    author_email='carloscesar182@gmail.com',
+    url='https://github.com/carloscesar182',
+    description='A simple printer of nested files'
+)
