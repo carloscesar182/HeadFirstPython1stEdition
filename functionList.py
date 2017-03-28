f76476462c572f3805af3492308517ae7cba14db
carloscesar182 <carloscesar182@gmail.com>
2017-03-27 21:45:05 -0300

    Update on Nester function - v1.1.0

--- functionList.py
+++ functionList.py
@@ -1,7 +1,9 @@
-from nester import *
+import nester
 
 movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
           ["Graham Chapman",
            ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
-print_lol(movies)  # Invoke the function 'print_lo' inside the 'nester.py' module
+cast = ["Palin", "Cleese", "Idle", "Jones", "Gilliam", "Chapman"]
+
+nester.print_lol(movies, 0)  # Invoke the function 'print_lol' inside the 'nester.py' module
--- nester.py
+++ nester.py
@@ -1,11 +1,15 @@
 """This is the 'nester.py' module, and it provides a function called
 print_lol() which prints lists that may or may not include nested lists."""
-def print_lol(the_list):
+def print_lol(the_list, level):
     """This function takes a positional argument called 'the_list', which is any
     Python list (of, possibly, nested lists). Each data item in the provided list
-    is (recursively) printed on the screen on its own line."""
+    is (recursively) printed on the screen on its own line.
+    A second argument called 'level' is used to insert tab-stops"""
+
     for each_item in the_list:  # Process the provided list with a 'for' loop
         if isinstance(each_item, list):  # If the item is being processed is itself a list, invoke the function
-            print_lol(each_item)
+            print_lol(each_item, level+1)  # Give the second argument to the function plus 1 each time it run
         else:
+            for tab_stop in range(level):  # Use the value of 'level' to control how many tab-stops are used
+                print("\t", end='')  # Display a TAB character for each level of indentation
             print(each_item)  # If the item is being processed ISN´T a list, display the item on screen
